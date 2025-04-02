a95b33010e97ddfeb533e38a66aa2674213cf20d
NguyenNhutY <130396826+NguyenNhutY@users.noreply.github.com>
2025-04-02 16:49:15 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/UID-Interior-Design/', // Đường dẫn repo của bạn
   chunkSizeWarningLimit: 1000, // Thay đổi giá trị này để phù hợp với yêu cầu của bạn
   server: {
     port: 3000, // Cổng mà bạn muốn sử dụng cho server phát triển
