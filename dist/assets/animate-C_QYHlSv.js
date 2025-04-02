1a57cd23cbd6b4ac308e76a6f7e1006f8e5c17b2
NguyenNhutY <130396826+NguyenNhutY@users.noreply.github.com>
2025-04-02 16:45:07 +0700

    Deploy Vite app

---
+++ dist/assets/animate-C_QYHlSv.js
@@ -0,0 +1,2 @@
+const t=i=>({initial:{y:50,opacity:0},animate:{y:0,opacity:1,transition:{duration:.5,delay:i}}}),a=i=>({initial:{x:50,opacity:0},animate:{x:0,opacity:1,transition:{duration:.5,delay:i}}});export{t as S,a};
+//# sourceMappingURL=animate-C_QYHlSv.js.map
